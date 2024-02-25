ca937453f272892ec03ce86e799b2b3a1fd2d01e
daniel-aizcorbe <82110164+Daniel-Aizcorbe@users.noreply.github.com>
2024-02-25 12:56:33 -0300

    add modal al crear nota

--- src/components/CrearNota/content/CreateButton.js
+++ src/components/CrearNota/content/CreateButton.js
@@ -1,12 +1,14 @@
 import { PlusOutlined } from '@ant-design/icons'
 import { Button, Flex } from 'antd'
-import React from 'react'
+import React, { useState } from 'react'
 import { useDispatch } from 'react-redux'
 import { createNote } from '../../../redux/slices/notasSlice'
+import ModalNotaCreada from './ModalNotaCreada'
 
 export const CreateButton = ({ nextId, title, content, selectedTags }) => {
 
     const dispatch = useDispatch();
+    const [openModal, setOpenModal] = useState(false);
 
     const create = () => {
         const nuevaNota = {
@@ -18,13 +20,18 @@ export const CreateButton = ({ nextId, title, content, selectedTags }) => {
         }
 
         dispatch(createNote(nuevaNota));
+        setOpenModal(true);
     }
+
     return (
         <Flex
             justify='center'
             align='center'
             style={{ width: "100%" }}
         >
+            <ModalNotaCreada
+                open={openModal}
+            />
             <Button
                 type='primary'
                 icon=<PlusOutlined />
